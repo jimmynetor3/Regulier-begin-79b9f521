276aeaf6af2ae93403ad4cd2282c6609ea80460c
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-16 09:24:12 +0200

    Uitwerking van de exercise

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,13 +1,25 @@
 <?php
 $pattern = '/[0-9]{10}/m';
 
-$input = readline("telnummer pls");
+$input = $argv[1];
 
-if (preg_match($pattern, $input))
-{
-    echo("$input is een geldig nummer");
+if (isset($input))
+ {
+
+	if (preg_match($pattern, $input))
+	{
+	    echo("$input is een geldig nummer");
+	}
+	else
+	{
+	    echo("$input is niet een geldig nummer");
+	}
+
 }
 else
 {
-    echo("$input is niet een geldig nummer");
+
+	echo("no input");
+
 }
+
